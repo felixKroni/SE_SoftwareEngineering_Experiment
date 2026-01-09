06dcf38fd812cde0ede5e233399ed54215141997
felixKroni <felixkronsteiner@gmx.at>
2026-01-09 19:10:36 +0100

    fixed issue with ci

--- frontend/SE_Experiment_Frontend/src/app/app.ts
+++ frontend/SE_Experiment_Frontend/src/app/app.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {
+export class App {
   protected readonly title = signal('SE_Experiment_Frontend');
 }
